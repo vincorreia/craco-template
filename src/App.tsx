75911480220824623d00db2f6934dda40c23ac05
vincorreia <vcorreiaes@gmail.com>
2022-11-06 02:06:10 +0100

    [HOTFIX]: Configure Tailwind & postcss Add: + tailwind.config.js + postcss.config.js

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 		<div className='App'>
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
-				<p className='text-red'>
+				<p className='text-red-700'>
 					Edit <code>src/App.tsx</code> and save to reload.
 				</p>
 				<a
